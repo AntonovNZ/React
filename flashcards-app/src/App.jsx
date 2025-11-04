6d0d80296af0cbf4754c77e9255f7e26f1b5c565
nezh <randomnameiscoolokay@gmail.com>
2025-11-04 15:00:00 +1100

    change tsx to jsx

---
+++ flashcards-app/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react'
+
+
+export function App() {
+
+  const [savedText, setSavedText] = useState([])
+  const [text, setText] = useState("")
+
+
+  const addCard = () => {
+  }
+  // can do this later
+  useEffect(() =>
+    setSavedText(savedText.push(text))
+
+  ), [text]
+
+  const onChangeTextArea = (event) => setText((event) => event.target.value)
+
+  return (
+    <>
+      <input type="text" value={text} onChange={onChangeTextArea}></input>
+      <button onClick={addCard}>
+      </button>
+    </>
+  )
+}
+
+
